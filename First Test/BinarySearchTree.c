93d153c042a86e7598398defcade40ad04d6c959
Lorenzo Bandini <lboa@hotmail.it>
2022-10-25 17:16:30 +0200

    BST

--- First Test/BinarySearchTree.c
+++ First Test/BinarySearchTree.c
@@ -1,3 +1,13 @@
+/*
+Scrivere un programma che legge da standard input un array di interi, e, utilizzando una funzione, crea un secondo array con le somme delle copie adiacenti di elementi. Il programma stampa il secondo array ottenuto.
+
+Per un array a di lunghezza n il risultato del calcolo è un array b di lunghezza n-1 dove b[i]=a[i]+a[i+1] . Nel caso in cui n<2 il programma non stampa niente.
+
+Il programma deve fare il controllo dell'input: se l'utente non inserisce un intero valide viene stampato il messaggio "Errore in input, inserisci un intero valido\n" e viene ripetuta la lettura da standard input.
+
+L'input contiene prima il numero di elementi dell'array e poi gli elementi stessi. In output l'array viene stampato un elemento per riga.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
